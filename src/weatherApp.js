62b57750312cbc917a4adc630a935191e78f5e3a
Weiss Bai <whitesgr03@gmail.com>
2022-12-26 22:26:45 +0800

    Update weatherApp script

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -3,7 +3,7 @@
 import "./css/style.css";
 
 import { weatherApi } from "./components/handleWeatherApi";
-
+import { countryList } from "./components/handleCountryData";
 import { main } from "./layouts/main";
 
 const createWeatherApp = () => {
